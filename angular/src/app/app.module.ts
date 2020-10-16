91a12b2766458b318acca6b277c863de5c8359ac
karavi home <karavi@ntk.ir>
2020-10-16 11:07:35 +0330

    optimaze

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule, HttpClient } from "@angular/common/http";
-import { CmsService } from './cmsService/cms.service';
-import { FileUploadModule } from 'ng2-file-upload';
-
+import { CmsService } from "./cmsService/cms.service";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FileUploadModule,
     FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-
-
-    
   ],
   bootstrap: [AppComponent],
-  providers: [CmsService]
+  providers: [CmsService],
 })
 export class AppModule {}
--- angular/src/app/modules/popup/popup.module.ts
+++ angular/src/app/modules/popup/popup.module.ts
@@ -3,19 +3,10 @@ import { NgModule } from "@angular/core";
 import { PopupComponent } from "./pages/popup/popup.component";
 import { PopupRoutingModule } from "./popup-routing.module";
 import { FormsModule } from "@angular/forms";
-import { FileUploadModule } from 'ng2-file-upload';
-
-
+import { FileUploadModule } from "ng2-file-upload";
 
 @NgModule({
-  declarations: [PopupComponent,],
-  imports: [
-    CommonModule,
-    PopupRoutingModule,
-    FormsModule,
-  //  TabsModule
-  FileUploadModule 
-  ],
+  declarations: [PopupComponent],
+  imports: [CommonModule, PopupRoutingModule, FormsModule, FileUploadModule],
 })
-
 export class PopupModule {}
